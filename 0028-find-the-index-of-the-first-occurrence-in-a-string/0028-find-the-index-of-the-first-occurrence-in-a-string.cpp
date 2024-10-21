a76246d5bd38d78f3f6ce8aaa4fedc738be75d4d
Pichika Ganesh <nagpichikaganesh@gmail.com>
2024-10-21 23:19:50 +0530

    Time: 0 ms (100%), Space: 7.9 MB (65.21%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -5,14 +5,15 @@ public:
             return 0;
         }
 
-        int h_len = haystack.length();
-        int n_len = needle.length();
-        for(int i = 0; i <= h_len - n_len; ++i){
-            string sub_string = haystack.substr(i, n_len);
-            if(sub_string == needle){
-                return i;
-            }
-        }
-        return -1;
+        // int h_len = haystack.length();
+        // int n_len = needle.length();
+        // for(int i = 0; i <= h_len - n_len; ++i){
+        //     string sub_string = haystack.substr(i, n_len);
+        //     if(sub_string == needle){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+        return haystack.find(needle);
     }
 };
